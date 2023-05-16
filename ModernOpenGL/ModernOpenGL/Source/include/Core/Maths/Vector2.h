7956d4436394dbcdf221a6b12de45db761435888
Eole TRIER <e.trier@student.isartdigital.com>
2023-05-16 14:14:35 +0200

    Shader class added + texture brut code added

--- ModernOpenGL/ModernOpenGL/Source/include/Core/Maths/Vector2.h
+++ ModernOpenGL/ModernOpenGL/Source/include/Core/Maths/Vector2.h
@@ -1,1 +1,16 @@
 #pragma once
+
+class Vector2
+{
+public:
+	float x, y;
+
+	Vector2() : x(0.0f), y(0.0f) {}
+	Vector2(float xy) : x(xy), y(xy){}
+	Vector2(float x, float y) : x(x), y(y) {}
+
+	Vector2 operator*(float mult) const { return Vector2{ x * mult, y * mult }; }
+	Vector2 operator/(float divi) const { return Vector2{ x / divi, y / divi }; }
+	Vector2 operator+(Vector2 v) const { return Vector2{ x + v.x, y + v.y }; }
+	Vector2 operator-(Vector2 v) const { return Vector2{ x - v.x, y - v.y }; }
+};
