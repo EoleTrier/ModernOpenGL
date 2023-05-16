cada1813c0d3188c5d8deb3ae831c74a6718d4ad
Eole TRIER <e.trier@student.isartdigital.com>
2023-05-16 16:00:15 +0200

    Log class added

--- ModernOpenGL/ModernOpenGL/Source/include/Core/Debug/Log.h
+++ ModernOpenGL/ModernOpenGL/Source/include/Core/Debug/Log.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include <filesystem>
+#include <fstream>
+
+
+class Log
+{
+public:
+	Log() {};
+	~Log() {};
+
+	void OpenFile(std::filesystem::path const& filename);
+	void Print(const char* format, ...);
+};
---
+++ ModernOpenGL/ModernOpenGL/Source/src/Core/Debug/Log.cpp
@@ -0,0 +1,11 @@
+#include "../../../../Source/include/Core/Debug/Log.h"
+
+void Log::OpenFile(std::filesystem::path const& filename)
+{
+
+}
+
+void Print(const char* format, ...)
+{
+
+}
